ab061e454c6e53356bd43c98c412666504375522
Gangland21 <landgang618@gmail.com>
2025-05-28 22:55:21 +0200

    Update settings.js

--- settings.js
+++ settings.js
@@ -6,9 +6,9 @@ const fs = require('fs')
 const chalk = require('chalk')
 
 //contact details
-global.ownernomer = "255697608274"
-global.ownername = "𝐷𝛥𝛮𝛮𝑌"
-global.ownernumber = '255697608274'  //owner number 2
+global.ownernomer = "260771798128"
+global.ownername = "☠︎︎ꨄ︎❦︎•Cyrus•❦︎ꨄ︎☠︎︎!"
+global.ownernumber = '260771798128'  //owner number 2
 //put your phone number above
 //let the owner as Danny or you can just put your name
 
